85bb17fc5e93d9193ef5da3b061316c35787f74b
Patrik Ken <kenfackjoslin@gmail.com>
2021-09-24 19:06:54 +0300

    .

--- live_emotion_recognition.py
+++ live_emotion_recognition.py
@@ -15,29 +15,27 @@ def preprocess_image(image):
     image = image.astype("float32") / 255.0
     # subtract ImageNet mean, divide by ImageNet standard deviation,
     # set "channels first" ordering, and add a batch dimension
-    #image -= config.MEAN
-    #mage /= config.STD
     image = np.transpose(image, (2, 0, 1))
     image = np.expand_dims(image, 0)
     # return the preprocessed image
     return torch.from_numpy(image)
 
 
-model = torch.load('emotion_recognition/Trained_Model')
+model = torch.load('Trained_Model')
 model.eval()
 
 labels = ['Happyness', 'Neutral', 'Sadness',
           'Surprise']  # Fear, Anger, Contempt
-colors = [(0, 255, 0), (240, 240, 240), (0, 0, 255),  (255, 0, 255)]
+colors = [(0, 255, 0), (255, 240, 0), (0, 0, 255),  (255, 0, 255)]
 
-haar_cascade = cv.CascadeClassifier('emotion_recognition/haar_face.xml')
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
 capture = cv.VideoCapture(0)
 pTime = time.time()
 
 while True:
     isTrue, img = capture.read()
     face_rect = haar_cascade.detectMultiScale(
-        img)
+        img, minNeighbors=5)
     for (x, y, w, h) in face_rect:
         face_roi = img[y:y+h, x:x+w]
         resized_tensor = preprocess_image(face_roi)
@@ -45,8 +43,8 @@ while True:
         # print(label)
         cv.rectangle(img, (x, y), (x+w, y+h), colors[label], thickness=1)
         cv.putText(img, f'Emotion: {labels[label]}({prob.item():.2F})', (x, y-10),
-                   cv.FONT_HERSHEY_SIMPLEX, .7, colors[label])
-        #cv.imshow('RESIZED', resized_tensor[0].permute(1, 2, 0).numpy())
+                   cv.FONT_HERSHEY_DUPLEX, 1, colors[label])
+
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
